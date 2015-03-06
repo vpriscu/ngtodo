1d0797dccf59c6becb4ec337b7472b4f829aa4bf
vpriscu <1234sdfDFer23@>
2015-03-06 18:03:21 +0200

    added functionality to send friend requests

--- app/scripts/controllers/account.js
+++ app/scripts/controllers/account.js
@@ -8,8 +8,9 @@
  * Controller of the ngtodoApp
  */
 angular.module('ngtodoApp')
-    .controller('AccountCtrl', ['$scope', '$firebase', '$location', 'userHandler', function ($scope, $firebase, $location, userHandler) {
+    .controller('AccountCtrl', ['$scope', '$firebase', '$location', 'userHandler', '$filter', function ($scope, $firebase, $location, userHandler, $filter) {
       var ref = new Firebase('https://ngtodo-vlad.firebaseio.com/users');
+      var refFriend = new Firebase('https://ngtodo-vlad.firebaseio.com/friends');
       if (userHandler.AuthStatus() === false) {
         $location.path('/user');
       } else {
@@ -24,4 +25,40 @@ angular.module('ngtodoApp')
           username: account.username
         });
       };
+      $scope.searchFriend = function() {
+        if($scope.find.username.length > 2) {
+          $scope.find.foundFriends = $firebase(ref.orderByChild('username')).$asArray();
+        } else {
+          $scope.find.foundFriends = false;
+        }
+      };
+
+      // we get all the friends list for the current user.
+      $scope.friendsList =  $firebase(refFriend.orderByChild('uid').equalTo($scope.userData.uid)).$asArray();
+
+      $scope.addFriend = function(item) {
+        var requestPresent = $filter('filter')($scope.friendsList, {friendId: item.$id, approved: 0});
+        if(requestPresent.length === 0 && item.$id !== $scope.userData.uid){
+          refFriend.push({
+            approved: 0,
+            friendId: item.$id,
+            friendName: item.username,
+            uid: $scope.userData.uid
+          }, function(error) {
+            if(error === null) {
+              toast(item.username + ' will recive a friend request', 6000);
+            }
+          })
+        } else {
+          toast(' You already sent a request to ' + item.username, 6000);
+        }
+
+        $scope.find.username = '';
+        $scope.find.foundFriends = false;
+      };
+      $scope.removeFriend = function(id) {
+        var refFriendRemove = new Firebase('https://ngtodo-vlad.firebaseio.com/friends/'+id);
+        refFriendRemove.remove();
+      }
+      //console.log($scope.friendsList);
     }]);
--- app/scripts/directives/useraction.js
+++ app/scripts/directives/useraction.js
@@ -7,16 +7,20 @@
  * # userAction
  */
 angular.module('ngtodoApp')
-  .directive('userAction', function ($location) {
+  .directive('userAction', function ($location, $firebase) {
     return {
       restrict: 'E',
       templateUrl: '/views/directives/user-action.html',
       scope: true,
-      link: function postLink($scope, $location, userHandler) {
+      link: function($scope, $location, userHandler) {
+        var ref = new Firebase('https://ngtodo-vlad.firebaseio.com/users');
         $('#logout').on('click', function() {
-          var ref = new Firebase('https://ngtodo-vlad.firebaseio.com/users');
           ref.unauth();
         });
+        $scope.userData = ref.getAuth();
+        var refFriend = new Firebase('https://ngtodo-vlad.firebaseio.com/friends');
+
+        $scope.friendRequests = $firebase(refFriend.orderByChild('friendId').equalTo($scope.userData.uid)).$asArray();
       }
     };
   });
